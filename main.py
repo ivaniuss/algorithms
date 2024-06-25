6d897afc5f18f6fc251f371cdc1f21db5db6a065
ivaniuss <ivan.emi94@gmail.com>
2024-06-24 19:35:25 -0500

    insertion_sort

---
+++ main.py
@@ -0,0 +1,16 @@
+def insertion_algorithm(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+    return array
+
+def main():
+    array = [5, 2, 4, 6, 1, 3]
+    print(insertion_algorithm(array))
+
+if __name__ == "__main__":
+    main()
